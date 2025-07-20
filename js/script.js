8325d6b9d4ca9708f7a5199a4228d3a150ac4901
Mayruf Ahmed <mayrufahmed472@gmail.com>
2025-07-20 16:50:26 +0000

    bc

--- js/script.js
+++ js/script.js
@@ -96,14 +96,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Form submission
-    const contactForm = document.querySelector('.contact-form');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Thank you for your message! We will get back to you soon.');
-            this.reset();
-        });
-    }
+    // const contactForm = document.querySelector('.contact-form');
+    // if (contactForm) {
+    //     contactForm.addEventListener('submit', function(e) {
+    //         e.preventDefault();
+    //         alert('Thank you for your message! We will get back to you soon.');
+    //         this.reset();
+    //     });
+    // }
 
     // Sticky header on scroll
     window.addEventListener('scroll', () => {
