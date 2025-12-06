a8f026a306feb367c6e81dc8a730fbbfced09aad
Mayruf Ahmed <mayrufahmed472@gmail.com>
2025-12-06 19:34:00 +0000

    tt

---
+++ ui/headerAndFooter/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    fetch("header.html")
+        .then(res => res.text())
+        .then(data => document.getElementById("header").innerHTML = data);
+
+    fetch("footer.html")
+        .then(res => res.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+};
